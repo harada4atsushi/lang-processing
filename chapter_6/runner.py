0c60092f66152d7bf2202305a86e95d37d81f18a
a.harada <redhornet96@gmail.com>
2016-03-21 15:17:06 +0900

    modify

--- chapter_6/runner.py
+++ chapter_6/runner.py
@@ -1,22 +1,12 @@
 # -*- coding: utf-8 -
 import re
 
-#pattern = ".*[\.;:\?_!]."
-#pattern = "(.*\.)"
-#repatter = re.compile(pattern)
+pattern = "(?<=[\.;:\?\!])"
+repatter = re.compile(pattern)
 
-str = "aiueo kakikuleko hoge: hoge. waaaaaay. togetoge"
-contactInfo = 'Doe, John: 555-1212'
+for line in open('nlp.txt', 'r'):
+    list = repatter.split(line)
+    print list
 
-#list = repatter.findall(contactInfo)
-#match = re.search(r'(\w+), (\w+): (\S+)', contactInfo)
-list = re.findall('.+[\.]', str)
-print list
-
-# for line in open('nlp.txt', 'r'):
-#     list = repatter.findall(line)
-#     print list
-#     strings = repatter.split(line)
-#
-#     for str in strings:
-#         print str + "\n"
+    # for str in list:
+    #     print str + "\n"
